a8f2077913ba44a96bc1d6772401f4e42f7bbc2e
k03302 <unisonman660@gmail.com>
2025-03-16 23:48:47 +0900

    Add base pawn for units

---
+++ Source/ToonTank/Public/BasePawn.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BasePawn.generated.h"
+
+UCLASS()
+class TOONTANK_API ABasePawn : public APawn
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this pawn's properties
+    ABasePawn();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Base Pawn", meta = (AllowPrivateAccess = true))
+    class UCapsuleComponent *CapsuleComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Base Pawn", meta = (AllowPrivateAccess = true))
+    UStaticMeshComponent *BaseMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Base Pawn", meta = (AllowPrivateAccess = true))
+    UStaticMeshComponent *TurretMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Base Pawn", meta = (AllowPrivateAccess = true))
+    USceneComponent *ProjectileSpawnPoint;
+};
