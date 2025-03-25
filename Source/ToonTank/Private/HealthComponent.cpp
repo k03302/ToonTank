c66e44228bdbb7d29c6141244abbc25e6204c0df
k03302 <unisonman660@gmail.com>
2025-03-26 01:09:10 +0900

    Register callback for OntTakeAnyDamage

--- Source/ToonTank/Private/HealthComponent.cpp
+++ Source/ToonTank/Private/HealthComponent.cpp
@@ -1,34 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "HealthComponent.h"
 
 // Sets default values for this component's properties
 UHealthComponent::UHealthComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+    // ...
 }
-
 
 // Called when the game starts
 void UHealthComponent::BeginPlay()
 {
-	Super::BeginPlay();
-
-	// ...
-	
+    Super::BeginPlay();
+    Health = MaxHealth;
+    GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::TakeDamge);
 }
-
 
 // Called every frame
-void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+    // ...
 }
 
+void UHealthComponent::TakeDamge(AActor *DamagedActor, float Damage, const UDamageType *DamageType, class AController *InstigatedBy, AActor *DamageCauser)
+{
+    UE_LOG(LogTemp, Warning, TEXT("%s TakeDamge of %f"), *DamagedActor->GetName(), Damage);
+}
--- Source/ToonTank/Public/HealthComponent.h
+++ Source/ToonTank/Public/HealthComponent.h
@@ -6,23 +6,29 @@
 #include "Components/ActorComponent.h"
 #include "HealthComponent.generated.h"
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class TOONTANK_API UHealthComponent : public UActorComponent
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
-	UHealthComponent();
+public:
+    // Sets default values for this component's properties
+    UHealthComponent();
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+    // Called when the game starts
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+public:
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
 
-		
+private:
+    UPROPERTY(EditAnywhere)
+    float MaxHealth = 100.f;
+
+    float Health;
+
+    UFUNCTION()
+    void TakeDamge(AActor *DamagedActor, float Damage, const class UDamageType *DamageType, class AController *InstigatedBy, AActor *DamageCauser);
 };
