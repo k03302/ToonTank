e899ded4c85ba360fbe6635d26099f1619b5f20a
k03302 <unisonman660@gmail.com>
2025-03-29 01:31:56 +0900

    Complete TakeDamge

--- Source/ToonTank/Private/HealthComponent.cpp
+++ Source/ToonTank/Private/HealthComponent.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "HealthComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "ToonTanksGameMode.h"
 
 // Sets default values for this component's properties
 UHealthComponent::UHealthComponent()
@@ -30,5 +32,18 @@ void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActo
 
 void UHealthComponent::TakeDamge(AActor *DamagedActor, float Damage, const UDamageType *DamageType, class AController *InstigatedBy, AActor *DamageCauser)
 {
-    UE_LOG(LogTemp, Warning, TEXT("%s TakeDamge of %f"), *DamagedActor->GetName(), Damage);
+    Health = FMath::Clamp(Health - Damage, 0.0f, MaxHealth);
+    UE_LOG(LogTemp, Warning, TEXT("%s TakeDamge of %f. Current Health: %f"), *DamagedActor->GetName(), Damage, Health);
+
+    if (Health <= 0)
+    {
+        if (AToonTanksGameMode *GameMode = Cast<AToonTanksGameMode>(UGameplayStatics::GetGameMode(GetWorld())))
+        {
+            GameMode->ActorDied(GetOwner());
+        }
+        else
+        {
+            UE_LOG(LogTemp, Error, TEXT("Health Component has no reference to GameMode"));
+        }
+    }
 }
